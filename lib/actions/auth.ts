976ef1c1e8032d8aa75f01b83efdcac9dde46009
stormlycan <satstormlink@gmail.com>
2025-02-13 06:21:31 +0530

    addition of rate limit for singin page using redis

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -3,17 +3,28 @@
 import { signIn } from "@/auth";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
+import ratelimit from "@/lib/rateLimit";
 import { hash } from "bcryptjs";
 import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 
 export const signInWithCredentials = async (params: Pick<AuthCredentials, "email" | "password">) => {
     const {email, password} = params;
+
+
+    const ip =  (await headers()).get('x-forwarded-for') || "127.0.0.1";
+
+    const {success} = await ratelimit.limit(ip);
+
+    if (!success)  return redirect("/too-fast");
+    
     try {
         const result =  await signIn('credentials', {
             email, password, redirect: false
         })
-
+ 
         if (result?.error) {
             return { success: false, error: result.error}
         }
@@ -27,7 +38,11 @@ export const signInWithCredentials = async (params: Pick<AuthCredentials, "email
 export const signUp =  async (params: AuthCredentials) => {
     const {fullName, email, password, universityId, universityCard} = params;
 
-    // check id the user already exists
+    const ip =  (await headers()).get('x-forwarded-for') || "127.0.0.1";
+
+    const {success} = await ratelimit.limit(ip);
+
+    if (!success)  return redirect("/too-fast")
 
     const existingUser =  await db
         .select()
